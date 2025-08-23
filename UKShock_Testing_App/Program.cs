d0245cdaff972ec3f856031b029079e7f0f2dc23
jericotyler <Jeremytswink@gmail.com>
2025-08-23 01:03:52 -0400

    Coded basic json push commands

--- UKShock_Testing_App/Program.cs
+++ UKShock_Testing_App/Program.cs
@@ -1,3 +1,105 @@
 ﻿// See https://aka.ms/new-console-template for more information
-\\ API Token ynSwrRwrG2lzQQp7IoVRZd7tESK3B1XaLV82dqBD312fOO6kVFOX5oHWnDeQjOLs
-Console.WriteLine("Hello, World!");
+using System.Drawing;
+using System.Net.Http.Headers;
+
+Console.WriteLine("Sending a Beep to the Shockers");
+
+await OpenShock.SendCommand(OpenShock.GreenShockerID,"Sound",100,1000);
+Console.WriteLine($"Sending a Beep to the Green Shocker");
+await OpenShock.SendCommand(OpenShock.OrangeShockerID, "Sound", 100, 1000);
+Console.WriteLine("Sending a Beep to the Orange Shocker");
+await OpenShock.SendCommand(OpenShock.BlackShockerID, "Sound", 100, 1000);
+Console.WriteLine("Sending a Beep to the Black Shocker");
+await OpenShock.GetShockers();
+class OpenShock
+{
+
+    static public string GreenShockerID = $"0197463d-3da3-76ea-830b-54e465ebbb59";
+    static public string OrangeShockerID = $"01974644-8d29-7006-a33f-e4fee8e5b906";
+    static public string BlackShockerID = $"01974645-862c-7e98-9036-3d7aa3447e7b";
+
+    static string APIToken = "ynSwrRwrG2lzQQp7IoVRZd7tESK3B1XaLV82dqBD312fOO6kVFOX5oHWnDeQjOLs";
+    static string APIUserAgent = "UKShockMod/1.0 (migratorycreatuesllc@gmail.com)";
+    public static async Task SendCommand(string a = "Null", string b = "Stop", int c = 0, int z = 300)
+    {
+        string Result = "Error";
+        string ComID = a;
+        string ComType = b;
+        int ComInt = c;
+        int ComDur = z;
+        string CommandJSON = $$"""
+    {
+  "shocks": [
+    {
+      "id": "{{ComID}}",
+      "type": "{{ComType}}",
+      "intensity": {{ComInt}},
+      "duration": {{ComDur}},
+      "exclusive": true
+    }
+  ],
+  "customName": null
+}
+""";
+
+
+        var client = new HttpClient();
+        client.DefaultRequestHeaders.Add("User-Agent", APIUserAgent);
+        client.DefaultRequestHeaders.Add("Open-Shock-Token", APIToken);
+
+        var request = new HttpRequestMessage
+        {
+            Method = HttpMethod.Post,
+            RequestUri = new Uri("https://api.openshock.app/2/shockers/control"),
+            Content = new StringContent(CommandJSON)
+
+
+            {
+                Headers =
+        {
+            ContentType = new MediaTypeHeaderValue("application/json")
+
+        }
+            }
+        };
+
+        using (var response = await client.SendAsync(request))
+        {
+            response.EnsureSuccessStatusCode();
+            var body = await response.Content.ReadAsStringAsync();
+            //Console.WriteLine(body);
+            Result = body;
+        }
+        return;
+    }
+    public static async Task GetShockers()
+    {
+        var client = new HttpClient();
+        client.DefaultRequestHeaders.Add("User-Agent", APIUserAgent);
+        client.DefaultRequestHeaders.Add("Open-Shock-Token", APIToken);
+        var request = new HttpRequestMessage
+        {
+            Method = HttpMethod.Get,
+            RequestUri = new Uri("https://api.openshock.app/1/shockers/own"),
+        };
+        using (var response = await client.SendAsync(request))
+        {
+            response.EnsureSuccessStatusCode();
+            var body = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(body);
+        }
+
+
+        Console.Write("Don't Read This");
+    }
+}
+class Shocker
+{
+    public string Name = "Blank";
+    public string ID = "Blank";
+}
+
+class PiShock
+{
+
+}
