559ce8e91ee20b22d7916c9105a4f8b95e81dd67
jericotyler <Jeremytswink@gmail.com>
2025-08-23 21:06:47 -0400

    Added conf file for the API key to avoid publishing it. APIkeys and will be stored in \Conf\

--- UKShock_Testing_App/Program.cs
+++ UKShock_Testing_App/Program.cs
@@ -1,6 +1,14 @@
 ﻿//Test Code For Deving a UK Shocker Plugin
 using OpenShock;
 using PiShock;
+using System.Formats.Tar;
+using System.Reflection;
+
+//check and set the API key
+string FileResults = await StartupTasks.APIKeyCheck();
+Console.WriteLine(FileResults);
+if (FileResults != "OK") return;
+Console.ReadLine();
 
 Console.WriteLine("""
     Enter A Choice To Procceed:
@@ -40,13 +48,40 @@ switch (input)
 
 } 
 
-static string MakeConfig()
+ public class StartupTasks()
 {
-    string result = "Bleep";
+    //Check for API Keys
+    public static Task <string> APIKeyCheck()
+    {
+        string OSAPIKey;
+        string OSFile = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Conf\OpenShockAPI.conf");
+        Console.WriteLine(OSFile);
+        //String OSFile = "OpenShockAPI.conf";
+        if (File.Exists(OSFile) == false) return Task.FromResult("No File");
+        
+        else
+        {
+            StreamReader sr = new StreamReader(OSFile);
+            OSAPIKey = sr.ReadLine();
+            if (OSAPIKey == null) return Task.FromResult("File Empty");
+            else
+            {
+                //Read the first line of text
+                OpenShock.API.Token = OSAPIKey;
+                Console.WriteLine(OSAPIKey);
+            }
+            return Task.FromResult("OK");
+        }
 
-    return result;
+        
+    }
+    //Check for Configs
+
+    
+
+
+
+    
 }
-
-
 
 
